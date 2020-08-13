0d73068890138fd4af9ca175a3873fbba23f1d5f
C. Chang <chloechang416@gmail.com>
2020-08-13 00:07:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["dog", "cat", "cow", "sheep"]
-  add_element_to_end_of_array << "pig"
+  element = "pig"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
